f595233d90717e77887991d7d0a1d0fc69d2f4a3
JakubowskiA <arieljakubowski@gmail.com>
2019-08-15 15:31:40 -0400

    Added to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import BooksContainer from './components/BooksContainer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <BooksContainer />
     </div>
   );
 }
---
+++ src/components/BooksContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BooksContainer extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default BooksContainer
