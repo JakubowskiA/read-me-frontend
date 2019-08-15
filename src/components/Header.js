5b5217750b75d2add7705ab316a36ea6a4c577c1
JakubowskiA <arieljakubowski@gmail.com>
2019-08-15 15:25:42 -0400

    Added components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header=()=>{
+    return(
+    <div>
+        <h1>Read Me</h1>
+        <br/>
+        <nav>ADD NAV STUFF HERE</nav>
+    </div>
+    )
+}
+
+export default Header
