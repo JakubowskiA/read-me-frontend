1e5cccb7d6093d8e51edf910e5f19a9dba9283ea
JakubowskiA <arieljakubowski@gmail.com>
2019-08-19 09:42:49 -0400

    added welcome and new user pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import BooksContainer from './components/BooksContainer'
 import Welcome from './components/Welcome'
 
+
 function App() {
+  // login=(userLogin)=>{
+  //   // LOGIN
+  // }
 
   
   return (
