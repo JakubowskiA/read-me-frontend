de7e071162f9f04d28ad744a85f32fce02bcaa6f
eschechter <schechter.evan@gmail.com>
2019-08-19 09:22:50 -0400

    add functionality to nav-bar buttons

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
-import {Card} from 'semantic-ui-react';
-import {Image} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 
-class Book extends Component{
-    render(){
-        return(
+class Book extends Component {
+    render() {
+        console.log(this.props);
+
+        return (
             <Card className="book-card">
-                <br/>
-                <Image src={this.props.book.image} centered/>
-                <br/>
+                <br />
+                <Image src={this.props.book.image} centered />
+                <br />
                 <h2>{this.props.book.title}</h2>
                 <h3>By: {this.props.book.author}</h3>
-                <button onClick={()=> this.props.addToMyBooks(this.props.book)}>Add to My Books</button>
-                <button onClick={()=> this.props.showBookDetails(this.props.book)}>See Details</button>
+                <button onClick={() => this.props.addToMyBooks(this.props.book)}>Add to My Books</button>
+                <button onClick={() => this.props.showBookDetails(this.props.book)}>See Details</button>
 
             </Card>
         )
--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import SearchForm from './SearchForm'
 import SearchedBooks from './SearchedBooks';
@@ -13,97 +13,100 @@ import DisplayBook from './DisplayBook';
 const BASE_URL = "http://localhost:3000"
 const TEST_USER_ID = 1;
 
-class BooksContainer extends Component{
-    state={
-        searchedBooks:[],
-        myBooks:[],
+class BooksContainer extends Component {
+    state = {
+        searchedBooks: [],
+        myBooks: [],
         displayedBook: {},
     }
-    submitSearch=(input)=>{
+    submitSearch = (input) => {
         fetch(`${BASE_URL}/users/${TEST_USER_ID}/search/${input.searchInputAuthor}`)
-        .then(res=>res.json())
-        .then(data=>this.setState({searchedBooks: data}))
+            .then(res => res.json())
+            .then(data => this.setState({ searchedBooks: data }))
         // SUBMIT SEARCH FORM
     }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch(`${BASE_URL}/users/${TEST_USER_ID}/my_books`)
-        .then(res=>res.json())
-        .then(data=>this.setState({myBooks: data}))
+            .then(res => res.json())
+            .then(data => this.setState({ myBooks: data }))
     }
 
-    addToMyBooks=(book)=>{
+    addToMyBooks = (book) => {
         // console.log(book)
         fetch(`${BASE_URL}/user_books`,
-        {method: "POST",
-        headers: {
-           "Content-Type": "application/json",
-           "Accept": "application/json"
-       }, body: JSON.stringify({
-           user_id: 1,
-           book_id: book.id
-        })})
-        .then(resp => resp.json())
-        .then(book=>this.setState({myBooks:[...this.state.myBooks, book]}))
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }, body: JSON.stringify({
+                    user_id: 1,
+                    book_id: book.id
+                })
+            })
+            .then(resp => resp.json())
+            .then(book => this.setState({ myBooks: [...this.state.myBooks, book] }))
 
         // ADD BOOK TO MY BOOKS
     }
 
-    showBookDetails=(book)=>{
+    showBookDetails = (book) => {
         // SHOW BOOK DETAILS
         // this.setState({displayedBook: book})
         // console.log(book)
         fetch(`${BASE_URL}/users/1/book_detail/${book.id}`)
-        .then(res => res.json())
-        .then(book => {
-            this.setState({displayedBook: book});
-            this.props.history.push('/display-book')
-        }
-        )
+            .then(res => res.json())
+            .then(book => {
+                this.setState({ displayedBook: book });
+                this.props.history.push('/display-book')
+            }
+            )
     }
-        
-        
 
-    render(){
-        return(
+
+
+    render() {
+        return (
             <div>
-                
+
                 <div className="component-row">
-                <Route
-                path="/search-books"
-                render={() => (
-                    <Fragment>
-                    <SearchForm submitSearch={this.submitSearch} />
-                    <SearchedBooks searchedBooks={this.state.searchedBooks} addToMyBooks={this.addToMyBooks} showBookDetails={this.showBookDetails}
+                    <Route
+                        path="/search-books"
+                        render={() => (
+                            <Fragment>
+                                <SearchForm submitSearch={this.submitSearch} />
+                                <SearchedBooks searchedBooks={this.state.searchedBooks} addToMyBooks={this.addToMyBooks} showBookDetails={this.showBookDetails}
+                                />
+                                <MyBooksSidebar myBooks={this.state.myBooks} />
+                            </Fragment>
+                        )}
                     />
-                    <MyBooksSidebar  myBooks={this.state.myBooks}/>
-                    </Fragment>
-                )}
-                />
-                <Route
-                path="/display-book"
-                render={() => (
-                    <Fragment>
-                    <SearchForm submitSearch={this.submitSearch} />
-                    <DisplayBook
-                        book={this.state.displayedBook}
+                    <Route
+                        path="/display-book"
+                        render={() => (
+                            <Fragment>
+                                <SearchForm submitSearch={this.submitSearch} />
+                                <DisplayBook
+                                    book={this.state.displayedBook}
+                                />
+                                <MyBooksSidebar myBooks={this.state.myBooks} />
+                            </Fragment>
+                        )}
+
                     />
-                    <MyBooksSidebar  myBooks={this.state.myBooks}/>
-                    </Fragment>
-                )}
 
-                />
+                    <Route
+                        path="/my-books"
+                        render={() => (
 
-                <Route
-                path="/my-books"
-                render={() => (
-                    
-                    <MyBooksPage  myBooks={this.state.myBooks} myBooks={this.state.myBooks} showBookDetails={this.showBookDetails}/>
-                
-                )}
+                            <MyBooksPage myBooks={this.state.myBooks}
+                                addToMyBooks={this.props.addToMyBooks}
+                                showBookDetails={this.showBookDetails} />
+                        )}
 
-                />
-                
+                    />
+
                 </div>
             </div>
         )
