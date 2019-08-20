b7d4237ef46220a45c1d4c9a0c1e6435a92c3e54
eschechter <schechter.evan@gmail.com>
2019-08-20 11:32:17 -0400

    make it so that the sign up button on the welcome page takes you to the sign up page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,53 +1,54 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Form } from 'semantic-ui-react';
 import { Card } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom'
 
-class Welcome extends Component{
-    state={
-        email:"",
-        password:""
+class Welcome extends Component {
+    state = {
+        email: "",
+        password: ""
     }
 
-    handleChangePassword = event =>{
-        this.setState({password:event.target.value})
+    handleChangePassword = event => {
+        this.setState({ password: event.target.value })
     }
 
-    handleChangeEmail = event =>{
-        this.setState({email:event.target.value})
+    handleChangeEmail = event => {
+        this.setState({ email: event.target.value })
     }
 
-render(){
-    return(
-        <div className="book-background">
-            <div className="login-card">
-                <div className="login-card-inner">
-            <Card >
-            <br/>
-            <h1>Sign In</h1>
-            <br/>
-            <Form onSubmit={() => this.props.login(this.state)} >
-                
-                    <Form.Field>
-                    <Form.Input placeholder="Enter your email address" onChange={this.handleChangeEmail} value={this.state.email}/>
-                    </Form.Field>
-                    <br/>
-                    <Form.Field>
-                    <Form.Input placeholder="Enter your password" onChange={this.handleChangePassword} value={this.state.password}/>
-                    </Form.Field>
-                
-                <br/>
-                <Form.Button>Submit</Form.Button>
-                </Form>
-                
-                <h3>New User?</h3>
-                <h3><a src='./NewUserForm'>Sign up here!</a></h3>
-                <br/>
-                </Card>
+    render() {
+        return (
+            <div className="book-background">
+                <div className="login-card">
+                    <div className="login-card-inner">
+                        <Card >
+                            <br />
+                            <h1>Sign In</h1>
+                            <br />
+                            <Form onSubmit={() => this.props.login(this.state)} >
+
+                                <Form.Field>
+                                    <Form.Input placeholder="Enter your email address" onChange={this.handleChangeEmail} value={this.state.email} />
+                                </Form.Field>
+                                <br />
+                                <Form.Field>
+                                    <Form.Input placeholder="Enter your password" onChange={this.handleChangePassword} value={this.state.password} />
+                                </Form.Field>
+
+                                <br />
+                                <Form.Button>Submit</Form.Button>
+                            </Form>
+
+                            <h3>New User?</h3>
+                            <h3><a onClick={_ => this.props.history.push('/sign-up')}>Sign up here!</a></h3>
+                            <br />
+                        </Card>
+                    </div>
                 </div>
-                </div>
-        </div>
-    )
-}
+            </div>
+        )
+    }
 }
 
-export default Welcome
+export default withRouter(Welcome)
